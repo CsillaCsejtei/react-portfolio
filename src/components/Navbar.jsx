364e1cc8407c32b5713c15af6725d12f87e45e08
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-10 19:19:39 +0100

    Created Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          My Portfolio
+        </Link>
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects" className="nav-link">
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
