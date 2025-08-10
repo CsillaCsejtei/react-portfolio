3209738aa12a3993dfd9daa26b1ad50d320d5b93
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-10 20:41:12 +0100

    Added Contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,43 @@
+export default function Contact() {
+  return (
+    <section className="container my-5 text-center">
+      <h2 className="mb-4">Let's Get in Touch!</h2>
+
+      <p>
+        <strong>Email:</strong>
+        <br />
+        <a
+          href="mailto:csillacsejtei@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          csillacsejtei@gmail.com
+        </a>
+      </p>
+
+      <p>
+        <strong>LinkedIn:</strong>
+        <br />
+        <a
+          href="https://www.linkedin.com/in/csilla-csejtei"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @csilla-csejtei
+        </a>
+      </p>
+
+      <p>
+        <strong>Instagram:</strong>
+        <br />
+        <a
+          href="https://www.instagram.com/csilla.csejtei"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @csilla.csejtei
+        </a>
+      </p>
+    </section>
+  );
+}
