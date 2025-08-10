a75bd478171a734e8f5e046748b3ecaec811dac1
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-10 19:30:29 +0100

    Created Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+export default function Home() {
+  return (
+    <div className="text-center">
+      <h1>Welcome to My Portfolio</h1>
+      <p className="lead">
+        With a decorated background in professional sports—including five
+        Hungarian Boxing Championship titles, two European Boxing Championship
+        bronze medals, and a European Kickboxing Championship bronze—I bring
+        focus, resilience, and a strong work ethic to my career in tech. After
+        transitioning from quality control in the vitamin manufacturing
+        industry, I followed my long-standing passion for technology and
+        graduated from SheCodes in May 2025 as a Front-End Developer. My studies
+        have covered Python, AI, HTML, CSS, JavaScript, Bootstrap, GitHub,
+        Netlify, React, Axios, responsive design, and more. 
+        I’m currently deepening my knowledge of back-end technologies through a full-stack
+        bootcamp, where I’m learning HTML, CSS, JavaScript, MySQL, Node, Python,
+        React, and deploying projects with Render—working toward becoming a
+        full-stack developer who builds thoughtful, user-focused digital
+        experiences.
+      </p>
+      <a
+        href="https://github.com/CsillaCsejtei"
+        className="btn btn-primary mt-3"
+      >
+        Visit My GitHub
+      </a>
+    </div>
+  );
+}
+
+ 
