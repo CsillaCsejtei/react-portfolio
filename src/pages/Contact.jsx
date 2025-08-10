1048c072836c8b56c19ac2b645bea3b950433c17
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-10 23:07:33 +0100

    Added style to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,43 +1,59 @@
+import React from 'react';
+
 export default function Contact() {
   return (
-    <section className="container my-5 text-center">
-      <h2 className="mb-4">Let's Get in Touch!</h2>
+    <section className="container my-5">
+      <h2 className="text-center mb-5">Let's Get in Touch!</h2>
 
-      <p>
-        <strong>Email:</strong>
-        <br />
-        <a
-          href="mailto:csillacsejtei@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          csillacsejtei@gmail.com
-        </a>
-      </p>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card p-4 shadow-sm">
+            <p className="mb-5 mt-4 text-center">
+              Feel free to reach out to me via any of the platforms below:
+            </p>
 
-      <p>
-        <strong>LinkedIn:</strong>
-        <br />
-        <a
-          href="https://www.linkedin.com/in/csilla-csejtei"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @csilla-csejtei
-        </a>
-      </p>
+            <div className="mb-5 text-center">
+              <strong>📧 Email:</strong>
+              <br />
+              <a
+                href="mailto:csillacsejtei@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-link"
+              >
+                csillacsejtei@gmail.com
+              </a>
+            </div>
 
-      <p>
-        <strong>Instagram:</strong>
-        <br />
-        <a
-          href="https://www.instagram.com/csilla.csejtei"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @csilla.csejtei
-        </a>
-      </p>
+            <div className="mb-5 text-center">
+              <strong>💼 LinkedIn:</strong>
+              <br />
+
+              <a
+                href="https://www.linkedin.com/in/csilla-csejtei"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-link"
+              >
+                @csilla-csejtei
+              </a>
+            </div>
+
+            <div className="mb-5 text-center">
+              <strong>📷 Instagram:</strong>
+              <br />
+              <a
+                href="https://www.instagram.com/csilla.csejtei"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-link"
+              >
+                @csilla.csejtei
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
