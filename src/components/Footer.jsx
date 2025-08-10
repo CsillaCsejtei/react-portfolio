de83b6fcc72265f3ef946f440f2fa21dff3e8a7a
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-10 19:20:16 +0100

    Created Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-auto">
+      <p className="mb-0">
+        This portfolio was coded by Csilla Csejtei and it is{" "}
+        <a
+          href="https://github.com/CsillaCsejtei/react-portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-light text-decoration-underline"
+        >
+          open-sourced
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
