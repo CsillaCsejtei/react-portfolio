0f76400625d159d1b729f807f999dcf9a15c8fef
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-10 18:57:22 +0100

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">PORTFOLIO
-    <footer>
-      This portfolio was coded by Csilla Csejtei and it is {" "} <a href="https://github.com/CsillaCsejtei/react-portfolio" target="_blank">
-      open-sourced </a>.
-    </footer>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contac" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
